6e3c33aed8943ec5f53c17c8c0a4216597aeac2e
aniknia <aniknia.dev@gmail.com>
2022-01-28 03:06:25 -0800

    chore: cleaned up repo

---
+++ telemetry.ino
@@ -0,0 +1,130 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <string.h>
+
+// Real Time Clock
+// RTC_PCF8523 RTC;
+
+// Analog Pins
+int analog0 = A0;
+
+// Digital Pins (for recording)
+int recording_toggle = 2;
+
+// Input Variables
+int input;
+
+// Recording Variables
+int recording;
+int last_recording = LOW;
+
+// File
+File logfile;
+char filename[] = "Tele00.txt";
+
+// SD Card
+const int chipSelect = 10;
+int sd;
+
+// Functions
+int check_sd(bool first_run=false, bool print_info=false) {
+  if(first_run) {
+    Serial.print("Initializing SD card... ");
+  }
+  else if(print_info) {
+    Serial.print("Checking SD card... ");
+  }
+
+  if(!SD.begin(chipSelect)) {
+    if(print_info) {
+      Serial.println("card failed, or not present");
+    }
+    return 0;
+  }
+  else {
+    if(print_info) {
+      Serial.println("card initialized.");
+    }
+    return 1;
+  }
+}
+
+void open_file() {
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[4] = i/10 + '0';
+    filename[5] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      logfile = SD.open(filename, FILE_WRITE);
+      break;
+    }
+  }
+}
+
+void close_file() {
+  logfile.close();
+}
+
+void print_serial(int input) {
+  Serial.println("Input: " + (String)input);
+}
+
+void print_file(int input) {
+  logfile.println("Input: " + (String)input);
+}
+
+int record_data(int input) {
+  print_file(input);
+  print_serial(input);
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  sd = check_sd(true, true);
+  while(!sd) {
+    sd = check_sd(false, true);
+    delay(10000);
+  }
+
+  // Setting Pins
+  pinMode(analog0, INPUT);
+  pinMode(recording_toggle, INPUT_PULLUP);
+
+  // Input Variables
+  input = analogRead(analog0);
+  recording = digitalRead(recording_toggle);
+}
+
+void loop() {
+  sd = check_sd();
+  if(sd) {
+    recording = digitalRead(recording_toggle);
+    input = analogRead(analog0);
+
+    if (last_recording != recording) {
+      if (recording == HIGH) {
+        last_recording = recording;
+        open_file();
+        Serial.println("Recording Starting");
+      }
+      else {
+        last_recording = recording;
+        close_file();
+        Serial.println("Recording Ending");
+      }
+    }
+    else if (recording == HIGH) {
+      record_data(input);
+    }
+    else {
+      // Do Nothing
+      // Serial.println("Recording Pending");
+    }
+    delay(1000);
+  }
+  else {
+    check_sd(false, true);
+    delay(10000);
+  }
+}
